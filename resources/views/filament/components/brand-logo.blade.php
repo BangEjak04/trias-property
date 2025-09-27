dc3d7a4fa6e92df44adb8a55c99b16b09eda57ff
BangEjak04 <rezaandyahw@gmail.com>
2025-09-27 17:39:51 +0700

    1.1.1

--- resources/views/filament/components/brand-logo.blade.php
+++ resources/views/filament/components/brand-logo.blade.php
@@ -1,3 +1,4 @@
 <div class="flex items-center">
-    <img class="h-10" src="{{ asset('TriasPropertyLandscapeLogo.png') }}" alt="" srcset="">
+    <img class="h-10 dark:hidden" src="{{ asset('LightLogo.png') }}" alt="" srcset="">
+    <img class="h-10 hidden dark:block" src="{{ asset('DarkLogo.png') }}" alt="" srcset="">
 </div>
